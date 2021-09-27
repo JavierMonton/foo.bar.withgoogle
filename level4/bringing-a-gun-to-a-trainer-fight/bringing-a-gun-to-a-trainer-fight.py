4743231fa7bb2a4c756babdd065d3b48f5f9d3a7
JavierMonton <javier.monton@typeform.com>
2021-09-27 10:04:13 +0200

    New version using mirrors. Mirrors done

--- level4/bringing-a-gun-to-a-trainer-fight/bringing-a-gun-to-a-trainer-fight.py
+++ level4/bringing-a-gun-to-a-trainer-fight/bringing-a-gun-to-a-trainer-fight.py
@@ -1,84 +1,71 @@
+import math
+
+
 def solution(dimensions, your_position, trainer_position, distance):
 
     width = dimensions[0]
     height = dimensions[1]
+    total = 0
 
     me = (your_position[0], your_position[1])
     trainer = (trainer_position[0], trainer_position[1])
 
-    p = generate_possibilities(width, height)
-    total = 0;
-    directions = []
-    for direction in p:
-        first_step = step(me, direction, width, height)
-        found = follow_path(first_step, me, trainer, width, height, direction, distance - 1)
-        if found is True:
-            total += 1
-            directions.append(direction)
+    jump_x1 = trainer[0] * 2  # left
+    jump_x2 = (width - trainer[0]) * 2  # right
 
-    print(directions)
+    jump_y1 = trainer[1] * 2  # down
+    jump_y2 = (width - trainer[0]) * 2  # up
+
+    mirror_x = math.ceil(distance / width) + 2
+    mirror_y = math.ceil(distance / height) + 2
+
+    trainer_locations = []
+
+    # build mirrors
+    x = trainer[0]
+    i = 0
+    for a in range(1, mirror_x + 1):
+        y = trainer[1]
+        j = 0
+        for b in range(1, mirror_y + 1):
+            trainer_locations.append((x, y))
+            trainer_locations.append((-x, y))
+            trainer_locations.append((x, -y))
+            trainer_locations.append((-x, -y))
+            if j % 2 == 1:
+                y = y + jump_y1
+            else:
+                y = y + jump_y2
+            j += 1
+
+        if i % 2 == 1:
+            x = x + jump_x1
+        else:
+            x = x + jump_x2
+        i += 1
+
+    print(trainer_locations)
+
+    # check distance + valid shot
+    for option in trainer_locations:
+        distance_x = option[0] - me[0]
+        distance_y = option[1] - me[1]
+        if math.sqrt(math.pow(distance_x, 2) + math.pow(distance_y, 2)) < distance:
+            print("(" + str(distance_x) + ", " + str(distance_y) + ")")
+            total += 1
+
     return total
 
 
 
-def generate_possibilities(width, height):
-    """
-    Given an scenario, creates an array of possibilities where the trainer can shot
-    """
-    p = []
-    p.append((0, 1))
-    p.append((1, 0))
-    for i in range(1, width+1):
-        for j in range(1, height+1):
-            if check_repeated(i, j) is True:
-                p.append((i, j))
-                p.append((-i, j))
-                p.append((i, -j))
-                p.append((-i, -j))
-    return p
 
 
-def check_repeated(x, y):
-    return x == 1 or y == 1 or (x % y != 0 and y % x != 0)
 
 
-def follow_path(start_point, me, goal, width, height, direction, distance, found = False):
-    """
-    Follow the path of a bullet
-    """
-    if start_point == goal:
-        return True
-    if start_point == me or distance == 0:  # we should skip first iteration of this outside of this function
-        return False
-    ####
-    return follow_path(step(start_point, direction, width, height), me, goal, width, height, direction, distance - 1, found)
 
 
-def step(a, b, width, height):
-    """
-    Makes a move on tuples, using walls or not
-    :param a: start point
-    :param b: direction
-    :param width: max width
-    :param height: max height
-    :return: tuple with new location
-    """
-    x = a[0] + b[0]
-    y = a[1] + b[1]
-    if x > width:
-        tmp = x - width
-        x = width - tmp
-    if y > height:
-        tmp = y - height
-        y = height - tmp
-    if x < 1:
-        x = abs(x)
-    if y < 1:
-        y = abs(y)
 
-    return (x, y)
-
-
+#print(solution([10, 10], [3, 3], [6, 6], 20))
 print(solution([3, 2], [1, 1], [2, 1], 4))  # 7
 print(solution([300, 275], [150, 150], [185, 100], 500))  # 9
 #print(solution([1250, 1250], [150, 150], [185, 100], 500))  # 9
