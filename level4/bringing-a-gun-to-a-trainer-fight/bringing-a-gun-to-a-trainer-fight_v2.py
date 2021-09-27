c79a97d1437ed8b509c0cb2965714b2f71d6174f
JavierMonton <javier.monton@typeform.com>
2021-09-27 21:53:54 +0200

    passing all tests except 3. Added a weird case with Fractions of 0

--- level4/bringing-a-gun-to-a-trainer-fight/bringing-a-gun-to-a-trainer-fight_v2.py
+++ level4/bringing-a-gun-to-a-trainer-fight/bringing-a-gun-to-a-trainer-fight_v2.py
@@ -70,10 +70,29 @@ def solution(dimensions, your_position, trainer_position, distance):
         distance_y = t[1] - me[1]
         total_distance = math.sqrt(math.pow(distance_x, 2) + math.pow(distance_y, 2))
 
-        if total_distance < distance:
-            #print(str(distance_x) + ", " + str(distance_y))
+        if total_distance <= distance:
+            (str(distance_x) + ", " + str(distance_y))
+            i = -1
             for t in me_locations:
+                i += 1
                 dir = (t[0] - me[0], t[1] - me[1])
+
+                # case 0,8 -> 0,-2
+                #if distance_x == 0 and dir[0] == 0
+                if distance_y != 0 and dir[1] != 0 and abs(dir[0]) <= abs(distance_x) and abs(dir[1]) <= abs(distance_y) and (distance_x != dir[0] or distance_y != dir[1]) and Fraction(distance_x, distance_y) == Fraction(dir[0], dir[1]) and (distance_x != dir[0]*-1 or distance_y != dir[1]*-1):
+                    if distance_x == 0 and dir[0] == 0 and ((distance_y > 0 and dir[1] < 0) or (dir[1 < 0 and distance_y > 0])):
+                        #print("weird case")
+                        total = total
+                    else:
+                        #("got suicide on " + str(dir[0]) + ", " + str(dir[1]))
+                        total -= 1
+                        break
+                elif distance_y == 0 and dir[1] == 0 and dir[0] != 0 and abs(dir[0]) <= abs(distance_x) and (distance_x != dir[0] or distance_y != dir[1]) and Fraction(distance_y, distance_x) == Fraction(dir[1], dir[0]):  # inverted Fraction to avoid division by 0
+                    #print(" -- got suicide on " + str(dir[0]) + ", " + str(dir[1]))
+                    total -= 1
+                    break
+                # move this to a function, it's repeated
+                dir = (trainer_locations[i][0] - me[0], trainer_locations[i][1] - me[1])
                 if distance_y != 0 and dir[1] != 0 and abs(dir[0]) <= abs(distance_x) and abs(dir[1]) <= abs(distance_y) and (distance_x != dir[0] or distance_y != dir[1]) and Fraction(distance_x, distance_y) == Fraction(dir[0], dir[1]) and (distance_x != dir[0]*-1 or distance_y != dir[1]*-1):
                     #print("got repeated on " + str(dir[0]) + ", " + str(dir[1]))
                     total -= 1
@@ -91,4 +110,22 @@ def solution(dimensions, your_position, trainer_position, distance):
 print(solution([3, 2], [1, 1], [2, 1], 4))  # 7
 print(solution([300, 275], [150, 150], [185, 100], 500))  # 9
 print(solution([2, 5], [1, 2], [1, 4], 11)) # 27
+print(solution([2, 3], [1, 2], [1, 1], 4))  # 7
+
+#print(solution([23, 10], [6, 4], [3, 2], 23))  # 8
+#print(solution([3, 2], [1, 1], [2, 1], 4))  # 7
+#print(solution([2, 3], [1, 2], [1, 1], 4))  # 7
+#print(solution([10, 10], [1, 1], [9, 9], 10))  # 0
+#print(solution([10, 5], [1, 1], [1, 3], 3))  # 2
+#print(solution([10, 2], [2, 1], [9, 1], 10))  # 5
+#print(solution([3, 2], [1, 1], [2, 1], 8))  # 7?
+#(solution([3, 2], [1, 1], [2, 2], 8))  # 4?
+#print(solution([3, 2], [1, 1], [2, 2], 1))  # 0
+#print(solution([3, 3], [1, 1], [3, 3], 1))  # 0
+#print(solution([4, 4], [1, 1], [3, 3], 2))  # 0
+#print(solution([4, 4], [1, 1], [3, 3], 3))  # 1?
+#print(solution([3, 3], [1, 1], [1, 2], 1))  # 1
+print(solution([10, 10], [1, 1], [1, 9], 8))  # 1
+
+#print(solution([10, 10], [4, 4], [3, 3], 5000))  # 739323?
 # print(solution([1250, 1250], [150, 150], [185, 100], 500))  # 9
